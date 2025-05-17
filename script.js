0e593e49ac7275536764327202fc7ac9161d9ff5
Milan-777 <milankumarhalder95@gmail.com>
2025-05-17 19:09:06 +0530

    add index.html as default

---
+++ script.js
@@ -0,0 +1,18 @@
+//scroll functons...
+function scrol(){
+    window.scrollTo({top: 0, behavior: "smooth"});
+}
+
+window.onscroll= function(){
+    if(window.scrollY > 1000){
+        document.getElementById('gototop').style.display= 'block';
+    }else{
+        document.getElementById('gototop').style.display= 'none';
+    }
+}
+
+//line visibility...
+function line(){
+    document.getElementById('lin1').style.width='250px';
+    document.getElementById('lin2').style.width='250px';
+}
